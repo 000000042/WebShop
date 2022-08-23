bdeda425ef796b6dff923221b98cfc635c15b2c2
Mhosseinweb <hossein711@gmail.com>
2022-08-23 22:55:59 +0430

    0.04

---
+++ WebShop/Webshop.Core/Security/PasswordHelper.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Webshop.Core.Security
+{
+    public static class PasswordHelper
+    {
+
+        public static string EncodePasswordMd5(string pass) //Encrypt using MD5
+        {
+            Byte[] originalBytes;
+            Byte[] encodedBytes;
+            MD5 md5;
+            //Instantiate MD5CryptoServiceProvider, get bytes for original password and compute hash (encoded password)
+            md5 = new MD5CryptoServiceProvider();
+            originalBytes = ASCIIEncoding.Default.GetBytes(pass);
+            encodedBytes = md5.ComputeHash(originalBytes);
+            //Convert encoded bytes back to a 'readable' string
+            return BitConverter.ToString(encodedBytes);
+        }
+
+        public static byte[] SaltGenerator()
+        {
+            // generate a 128-bit salt using a cryptographically strong random sequence of nonzero values
+            byte[] salt = new byte[128 / 8];
+            using (var rngCsp = new RNGCryptoServiceProvider())
+            {
+                rngCsp.GetNonZeroBytes(salt);
+            }
+
+            return salt;
+        }
+
+        public static string EncodePassword(string password, byte[] salt)
+        {
+
+            // derive a 256-bit subkey (use HMACSHA256 with 100,000 iterations)
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 100000,
+                numBytesRequested: 256 / 8));
+
+            return hashed;
+        }
+
+    }
+}
---
+++ WebShop/Webshop.Core/Security/PermissionCheckerAttribute.cs
@@ -0,0 +1,42 @@
+﻿using Webshop.Core.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Claims;
+
+namespace Webshop.Core.Security
+{
+    public class PermissionCheckerAttribute : AuthorizeAttribute, IAuthorizationFilter
+    {
+        private int _permissionId = 0;
+        private IPermissionService _permissionService;
+
+        public PermissionCheckerAttribute(int permissionId)
+        {
+            _permissionId = permissionId;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                _permissionService =
+                    (IPermissionService)context.HttpContext.RequestServices.GetService(typeof(IPermissionService));
+
+                string userName = context.HttpContext.User.Identity.Name;
+
+                if (!_permissionService.CheckUserPermission(_permissionId, userName))
+                {
+                    context.Result = new RedirectResult("/Home?" + context.HttpContext.Request.Path);
+                }
+            }
+            else
+            {
+                context.Result = new RedirectResult("/Login" + "?ReturnPath=" + context.HttpContext.Request.Path);
+            }
+        }
+    }
+}
