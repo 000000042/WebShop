fea2aae4c2d1ba7c6f0426658c12b138590ca15d
Mhosseinweb <hossein711@gmail.com>
2022-08-24 09:04:27 +0430

    014

---
+++ WebShop/Webshop.DataLayer/Entities/User/UserDiscount.cs
@@ -0,0 +1,30 @@
+﻿using Webshop.DataLayer.Entities.Order;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Webshop.DataLayer.Entities.User
+{
+    public class UserDiscount
+    {
+        [Key]
+        public int UD_Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public int DiscountId { get; set; }
+
+
+        #region Navigation Props
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+        [ForeignKey("DiscountId")]
+        public Discount Discount { get; set; }
+
+        #endregion
+    }
+}
