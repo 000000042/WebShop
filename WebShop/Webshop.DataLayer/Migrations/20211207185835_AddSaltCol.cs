e79531760313ccf9e18d06df08d6c0985f5b27aa
Mhosseinweb <hossein711@gmail.com>
2022-08-24 09:06:32 +0430

    016

---
+++ WebShop/Webshop.DataLayer/Migrations/20211207185835_AddSaltCol.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Webshop.DataLayer.Migrations
+{
+    public partial class AddSaltCol : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Salt",
+                table: "Users",
+                nullable: false,
+                defaultValue: new byte[] {  });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Salt",
+                table: "Users");
+        }
+    }
+}
